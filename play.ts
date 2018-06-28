ff6a5227924e6b194c073d78cb3cec515923ce26
CR Drost <C.R.Drost@gmail.com>
2018-06-27 19:02:13 -0500

    chore: prettier

--- play.ts
+++ play.ts
@@ -1,31 +1,31 @@
 interface IUnit {
-  type: "unit";
+  type: 'unit';
   meta?: undefined;
 }
-const TUnit: IUnit = { type: "unit" };
+const TUnit: IUnit = { type: 'unit' };
 interface IText {
-  type: "text";
+  type: 'text';
   meta?: undefined;
 }
-const TText: IText = { type: "text" };
+const TText: IText = { type: 'text' };
 interface IReal {
-  type: "real";
+  type: 'real';
   meta?: undefined;
 }
-const TReal: IReal = { type: "real" };
+const TReal: IReal = { type: 'real' };
 interface IItem {
-  type: "item";
+  type: 'item';
   meta: SchemaDict;
 }
-function TItem<T extends SchemaDict>(dict: T): { type: "item"; meta: T } {
-  return { type: "item", meta: dict };
+function TItem<T extends SchemaDict>(dict: T): { type: 'item'; meta: T } {
+  return { type: 'item', meta: dict };
 }
 interface IEnum {
-  type: "enum";
+  type: 'enum';
   meta: SchemaDict;
 }
-function TEnum<T extends SchemaDict>(dict: T): { type: "enum"; meta: T } {
-  return { type: "enum", meta: dict };
+function TEnum<T extends SchemaDict>(dict: T): { type: 'enum'; meta: T } {
+  return { type: 'enum', meta: dict };
 }
 
 type Schema = IUnit | IText | IReal | IItem | IEnum;
@@ -38,23 +38,23 @@ interface IBaseTypes<T extends SchemaDict> {
   item: { [k in keyof T]: IValidated<T[k]> };
   enum<z>(handlers: { [k in keyof T]: (i: IValidated<T[k]>) => z }): z;
 }
-type IValidated<T extends Schema> = IBaseTypes<T["meta"]>[T["type"]];
+type IValidated<T extends Schema> = IBaseTypes<T['meta']>[T['type']];
 function assertValid<T extends Schema>(type: T, validated: IValidated<T>) {
   throw new Error();
 }
 
 assertValid(TReal, 123);
-assertValid(TText, "abc");
+assertValid(TText, 'abc');
 assertValid(
   TItem({
     abc: TReal,
     def: TText
   }),
-  { abc: 123, def: "456" }
+  { abc: 123, def: '456' }
 );
 const testEnum = TEnum({
   abc: TReal,
   def: TText
 });
 assertValid(testEnum, ({ abc }) => abc(123));
-assertValid(testEnum, ({ def }) => def("456"));
+assertValid(testEnum, ({ def }) => def('456'));
