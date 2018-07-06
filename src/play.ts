ebb8b8becadd6d399df014bbcf8c39b1e4f2f7ba
CR Drost <C.R.Drost@gmail.com>
2018-07-06 07:48:04 -0500

    Add type-variable schemas.

--- src/play.ts
+++ src/play.ts
@@ -12,21 +12,23 @@ function TEnum<T extends SchemaDict>(dict: T) {
   return { type: 'enum' as 'enum', value: dict };
 }
 
+interface IVarReference {
+  type: 'vref';
+  value: string;
+}
+
+type Schema = IPrimitive | IStructuredDict | IVarReference;
+type SchemaDict = { [k: string]: Schema };
+
+type EnumHelper<T extends SchemaDict, Env extends SchemaDict> = {
+  [k in keyof T]: { type: k; value: ValueOfType<T[k], Env> }
+};
+type Enum<T extends SchemaDict, Env extends SchemaDict> = EnumHelper<T, Env>[keyof T];
+
 interface IPrimitive {
   type: keyof IPrimMap;
   value: undefined;
 }
-
-interface IStructuredDict {
-  type: keyof IDictMap<any>;
-  value: SchemaDict;
-}
-
-type Schema = IPrimitive | IStructuredDict;
-type SchemaDict = { [k: string]: Schema };
-
-type EnumHelper<T extends SchemaDict> = { [k in keyof T]: { type: k; value: ValueOfType<T[k]> } };
-type Enum<T extends SchemaDict> = EnumHelper<T>[keyof T];
 
 interface IPrimMap {
   unit: undefined;
@@ -34,16 +36,34 @@ interface IPrimMap {
   real: number;
 }
 
-interface IDictMap<T extends SchemaDict> {
-  prod: { [k in keyof T]: ValueOfType<T[k]> };
-  enum: Enum<T>;
+interface IStructuredDict {
+  type: keyof IDictMap<any, any>;
+  value: SchemaDict;
 }
-type ValueOfType<T extends Schema> = T extends IStructuredDict
-  ? IDictMap<T['value']>[T['type']]
-  : T extends IPrimitive ? IPrimMap[T['type']] : never;
 
-function valueOfType<T extends Schema>(type: T): ValueOfType<T> {
-  throw new Error(String(type));
+interface IDictMap<T extends SchemaDict, Env extends SchemaDict> {
+  prod: { [k in keyof T]: ValueOfType<T[k], Env> };
+  enum: Enum<T, Env>;
+}
+
+interface IVarMap<Env extends SchemaDict> {
+  vref: { [k in keyof Env]: ValueOfType<Env[k], {}> };
+}
+
+type DictFreeMap<T extends SchemaDict> = { [k in keyof T]: FreeVars<T[k]> };
+
+type FreeVars<T extends Schema> = T extends IStructuredDict
+  ? DictFreeMap<T['value']>[keyof T['value']]
+  : T extends IVarReference ? T['value'] : never;
+
+type ValueOfType<T extends Schema, Env extends SchemaDict> = T extends IStructuredDict
+  ? IDictMap<T['value'], Env>[T['type']]
+  : T extends IPrimitive
+    ? IPrimMap[T['type']]
+    : T extends IVarReference ? IVarMap<Env>[T['type']][T['value']] : never;
+
+function valueOfType<T extends Schema, E extends SchemaDict>(type: T, env: E): ValueOfType<T, E> {
+  throw new Error(String(type) + env);
 }
 
 type subtype<A, B> = A extends B ? true : false;
@@ -52,8 +72,12 @@ function subtypeOf<A, B>(sub: A, sup: B): subtype<A, B> {
   throw new Error(String(sub) + sup);
 }
 
-function valid<T extends Schema, V>(type: T, value: V): V extends ValueOfType<T> ? true : false {
-  throw new Error(String(type) + value);
+function valid<T extends Schema, E extends SchemaDict, V>(
+  type: T,
+  value: V,
+  env: E
+): and<subtype<V, ValueOfType<T, E>>, subtype<E, Record<FreeVars<T>, Schema>>> {
+  throw new Error(String(type) + value + env);
 }
 
 type and<A extends boolean, B extends boolean> = A extends true
@@ -77,29 +101,29 @@ assertFalse(subtypeOf('abc', {} as object));
 assertFalse(subtypeOf(null as null, 123));
 assertFalse(subtypeOf(null as null, 123));
 
-assertTrue(valid(TUnit, undefined as undefined));
-assertFalse(valid(TUnit, null as null));
-assertFalse(valid(TUnit, 123));
-assertFalse(valid(TUnit, 'abc'));
+assertTrue(valid(TUnit, undefined as undefined, {}));
+assertFalse(valid(TUnit, null as null, {}));
+assertFalse(valid(TUnit, 123, {}));
+assertFalse(valid(TUnit, 'abc', {}));
 
-assertTrue(valid(TReal, 123));
-assertFalse(valid(TReal, 'abc'));
-assertFalse(valid(TReal, null as null));
-assertFalse(valid(TReal, undefined as undefined));
+assertTrue(valid(TReal, 123, {}));
+assertFalse(valid(TReal, 'abc', {}));
+assertFalse(valid(TReal, null as null, {}));
+assertFalse(valid(TReal, undefined as undefined, {}));
 
-assertTrue(valid(TText, 'abc'));
-assertFalse(valid(TText, 123));
-assertFalse(valid(TText, null as null));
-assertFalse(valid(TText, undefined as undefined));
+assertTrue(valid(TText, 'abc', {}));
+assertFalse(valid(TText, 123, {}));
+assertFalse(valid(TText, null as null, {}));
+assertFalse(valid(TText, undefined as undefined, {}));
 
 const testProduct = TProd({
   abc: TReal,
   def: TText
 });
-assertTrue(valid(testProduct, { abc: 123, def: '456' }));
-assertFalse(valid(testProduct, { abc: '123', def: 456 }));
-assertFalse(valid(testProduct, null as null));
-assertFalse(valid(testProduct, undefined as undefined));
+assertTrue(valid(testProduct, { abc: 123, def: '456' }, {}));
+assertFalse(valid(testProduct, { abc: '123', def: 456 }, {}));
+assertFalse(valid(testProduct, null as null, {}));
+assertFalse(valid(testProduct, undefined as undefined, {}));
 
 const testEnum = TEnum({
   abc: TReal,
@@ -108,10 +132,14 @@ const testEnum = TEnum({
 
 // The valid() function does not directly express V extends ValueOfType<T>, so it infers 'abc' (the
 // value) to have type `string` rather than type `'abc'`, then it gets these values incorrect.
-assertTrue(valid(testEnum, { type: 'abc' as 'abc', value: 123 }));
-assertTrue(valid(testEnum, { type: 'def' as 'def', value: '456' }));
-assertFalse(valid(testEnum, { type: 'abc' as 'abc', value: '123' }));
-assertFalse(valid(testEnum, { type: 'def' as 'def', value: 456 }));
+assertTrue(valid(testEnum, { type: 'abc' as 'abc', value: 123 }, {}));
+assertTrue(valid(testEnum, { type: 'def' as 'def', value: '456' }, {}));
+assertFalse(valid(testEnum, { type: 'abc' as 'abc', value: '123' }, {}));
+assertFalse(valid(testEnum, { type: 'def' as 'def', value: 456 }, {}));
+
+assertTrue(valid({ type: 'vref', value: 'abc' as 'abc' }, 'what', { abc: TText }));
+assertFalse(valid({ type: 'vref', value: 'abc' as 'abc' }, 'what', { def: TText }));
+assertFalse(valid({ type: 'vref', value: 'abc' as 'abc' }, 'what', {}));
 
 type KeySubtract<Q, R extends Q> = Q extends R ? never : Q;
 
